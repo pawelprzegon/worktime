0d278c53e2d9a648ca5525804e24d0f18fbcf423
pawelprzegon <p.przegon@gmail.com>
2024-11-17 22:43:29 +0100

    ref: rebuild shiftModal to get user access to change time

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,52 @@
+<script setup>
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import {ref, watch} from "vue";
+
+const emit = defineEmits(['newDatetime'])
+const selectedDate = ref(null);
+
+const formatDate = (date) => {
+  return date ? date.toISOString().split("T")[0] : "";
+};
+
+watch(selectedDate, (newValue, oldValue) => {
+  emit('newDatetime', newValue)
+});
+</script>
+
+<template>
+  <div class="minimal-datepicker">
+    <VueDatePicker
+      v-model="selectedDate"
+      :format="formatDate"
+      :hide-header="true"
+      :hide-months="true"
+      :hide-week-days="true"
+      placeholder="Pick"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+.minimal-datepicker .datepicker {
+  border: 1px solid #ddd;
+  padding: 5px;
+  border-radius: 5px;
+  background: white;
+  font-size: 14px;
+  width: 300px !important;
+}
+
+.minimal-datepicker .dp__header,
+.minimal-datepicker .dp__month_year_select,
+.minimal-datepicker .dp__weekdays {
+  display: none;
+}
+
+.minimal-datepicker .dp__cell {
+  padding: 8px;
+  font-size: 14px;
+}
+</style>
--- src/components/panel/ShiftDetailContainer.vue
+++ src/components/panel/ShiftDetailContainer.vue
@@ -2,7 +2,8 @@
 
 const props = defineProps({
   label: String,
-  data: String,
+  date: String,
+  time: String,
   background: String
 })
 
@@ -12,10 +13,12 @@ const props = defineProps({
 
 <template>
 
-  <div class="shift-detail"
-       :style="{background: props.background}">
-    <p>{{props.label}}:</p>
-    <h3>{{props.data}}</h3>
+  <div
+    :class="['shift-detail', label ? 'triple' : 'double']"
+    :style="{ background }">
+    <p v-if="label">{{ label }}:</p>
+    <h4>{{ date }}</h4>
+    <h3>{{ time }}</h3>
   </div>
 
 </template>
@@ -23,9 +26,9 @@ const props = defineProps({
 <style scoped>
 
 .shift-detail {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: 2fr 2fr 2fr;
+  justify-content: center;
   align-items: center;
   border-radius: 5px;
   padding: 1px;
@@ -33,14 +36,28 @@ const props = defineProps({
   width: 100%;
 }
 
+.double {
+  grid-template-columns: 2fr 2fr;
+}
+
+.triple {
+  grid-template-columns: 2fr 2fr 2fr;
+}
+
 .shift-detail p {
-  color: gray
+  color: gray;
+  font-size: 10px;
+}
+
+.shift-detail h4 {
+  padding: 2px;
+  width: 90%;
 }
 
 .shift-detail h3 {
   background: #3d3d3d;
   padding: 2px;
-  width: 50%;
+  width: 90%;
   border-radius: 3px;
 }
 
