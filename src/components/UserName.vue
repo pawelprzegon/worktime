8961f6b4dd3e507ea3140dcbb50d3f804aeb1af7
Pawel <p.przegon@gmail.com>
2024-12-21 19:03:50 +0100

    ref: UserName.vue tailwind portrait view

--- src/components/UserName.vue
+++ src/components/UserName.vue
@@ -8,14 +8,16 @@ const props = defineProps({
 </script>
 
 <template>
-  <section class="flex flex-col justify-center items-center gap-0 my-5">
+  <section class="flex flex-col justify-center items-center gap-0 my-5 portrait-medium:my-3 portrait-xs:my-2">
     <small
-      class="text-[15px] leading-[1.5] sm:text-[13px] md:text-[12px] lg:text-[11px] xl:text-[10px] portrait-medium:text-sm portrait-xs:text-xs"
+      class="text-[15px] leading-[1.5] sm:text-[13px] md:text-[12px] lg:text-[11px] xl:text-[10px]
+      portrait-medium:text-sm portrait-xs:text-xs"
     >
       {{ props.firstName }}
     </small>
     <small
-      class="text-[15px] leading-[1.5] sm:text-[13px] md:text-[12px] lg:text-[11px] xl:text-[10px] portrait-medium:text-sm portrait-xs:text-xs"
+      class="text-[15px] leading-[1.5] sm:text-[13px] md:text-[12px] lg:text-[11px] xl:text-[10px]
+      portrait-medium:text-sm portrait-xs:text-xs"
     >
       {{ props.lastName }}
     </small>
