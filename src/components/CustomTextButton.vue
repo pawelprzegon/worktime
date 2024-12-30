1b428f2b9e01ccdfd631f81a0bb43ffe16357019
pawelprzegon <p.przegon@gmail.com>
2024-12-30 20:11:15 +0100

    ref: Nav buttons tailwind style

--- src/components/CustomTextButton.vue
+++ src/components/CustomTextButton.vue
@@ -13,15 +13,19 @@ const props = defineProps({
 
 <template>
   <button
+      class="
+      flex items-center justify-center cursor-pointer box-border transition-all duration-200 text-sm rounded-md
+
+      portrait-2xs:w-15 portrait-2xs:h-8 portrait-2xs:text-2xs portrait-2xs:p-[3px] portrait-2xs:m-0
+      portrait-xs:w-15 portrait-xs:h-8 portrait-xs:text-xs portrait-xs:p-[2px] portrait-xs:m-0
+      portrait-small:w-30 portrait-small:h-8 portrait-small:text-sm portrait-small:p-2 portrait-small:m-2
+      portrait-medium:w-30 portrait-medium:h-8 portrait-medium:text-sm portrait-medium:p-2 portrait-medium:m-2
+      portrait-large:w-30 portrait-large:h-8 portrait-large:text-sm portrait-large:p-2 portrait-large:m-2
+      portrait-xl:w-30 portrait-xl:h-10 portrait-xl:text-base portrait-xl:p-2 portrait-xl:m-2
+      "
     :class="[
-      `flex items-center justify-center cursor-pointer box-border transition-all duration-200 text-sm rounded-md`,
       { 'bg-beb text-black': props.isActive, 'bg-mute': !props.isActive },
       { 'hover:bg-secondary hover:text-white hover:border-gray-300': !props.isActive },
-      'portrait-xs:w-23 portrait-xs:h-6 portrait-xs:font-sm portrait-xs:p-2 portrait-xs:m-1',
-      'portrait-small:w-23 portrait-small:h-7 portrait-small:font-sm portrait-small:p-2 portrait-small:m-2',
-      'portrait-medium:w-25 portrait-medium:h-8 portrait-medium:font-sm portrait-medium:p-2 portrait-medium:m-2',
-      'portrait-large:w-25 portrait-large:h-8 portrait-large:font-sm portrait-large:p-2 portrait-large:m-2',
-      'portrait-xl:w-28 portrait-xl:h-10 portrait-xl:font-base portrait-xl:p-2 portrait-xl:m-2'
     ]"
     type="button"
   >
