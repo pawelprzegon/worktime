1a4caae6ff7a2bd8a2808a8a4529bcef59136d3f
pawelprzegon <p.przegon@gmail.com>
2024-12-31 21:23:02 +0100

    ref: Calendar portrait-small grid gap change

--- src/components/ShadowBox.vue
+++ src/components/ShadowBox.vue
@@ -6,11 +6,11 @@
 
    <section
       class="
-      min-w-[200px] max-w-[80%] rounded-lg bg-mute p-2 m-2 shadow-xl h-fit
+      min-w-[200px] max-w-[80%] bg-mute p-2 m-2 shadow-xl h-fit
 
-      portrait-2xs:w-[200px] portrait-2xs:p-[1px] portrait-2xs:m-[1px]
+      portrait-2xs:w-[200px] portrait-2xs:p-[1px] portrait-2xs:m-[1px] portrait-2xs:rounded-sm
       portrait-xs:min-w-[180px] portrait-xs:max-w-[20%] portrait-xs:p-[1px] portrait-xs:m-[1px]
-      portrait-small:min-w-[150px] portrait-small:max-w-[150px] portrait-small:p-[1px] portrait-small:m-[1px]]
+      portrait-small:min-w-[150px] portrait-small:max-w-[150px] portrait-small:p-[1px] portrait-small:m-[1px]] portrait-small:rounded-lg
       portrait-medium:min-w-[200px] portrait-medium:max-w-[30%] portrait-medium:p-1 portrait-medium:m-1
       portrait-large:min-w-[250px] portrait-large:max-w-[40%]
       ">
--- src/pages/userPanel/calendar/Calendar.vue
+++ src/pages/userPanel/calendar/Calendar.vue
@@ -85,7 +85,7 @@ onMounted(async () => {
            below-portrait-2xs:gap-1 below-portrait-2xs:[grid-template-columns:repeat(1,minmax(200px,1fr))] below-portrait-2xs:w-max-[280px]
            portrait-2xs:gap-1 portrait-2xs:[grid-template-columns:repeat(1,minmax(200px,1fr))] portrait-2xs:w-max-[280px]
            portrait-xs:gap-1 portrait-xs:[grid-template-columns:repeat(1,minmax(300px,1fr))] portrait-xs:w-max-[380px]
-           portrait-small:gap-2 portrait-small:[grid-template-columns:repeat(7,minmax(55px,55px))] portrait-small:w-max-[440px]
+           portrait-small:gap-[5px] portrait-small:[grid-template-columns:repeat(7,minmax(55px,55px))] portrait-small:w-max-[440px]
            portrait-medium:gap-2 portrait-medium:[grid-template-columns:repeat(7,minmax(70px,70px))] portrait-medium:w-max-[600px]
            portrait-large:gap-2 portrait-large:[grid-template-columns:repeat(7,minmax(80px,80px))] portrait-large:w-max-[700px]
            portrait-xl:gap-4 portrait-xl:[grid-template-columns:repeat(7,minmax(90px,90px))] portrait-xl:w-max-[800px]
