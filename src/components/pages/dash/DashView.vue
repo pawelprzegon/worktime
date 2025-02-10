273d84f0ecb9dcf5ac604002d079484c3b0c2701
pawelprzegon <p.przegon@gmail.com>
2025-02-10 20:07:50 +0100

    ref: start, stop shift response alert

--- src/components/pages/dash/DashView.vue
+++ src/components/pages/dash/DashView.vue
@@ -1,41 +1,44 @@
 <script setup>
-import {getActiveShifts, getDashUsers} from "@/composables/fetchers.js";
-import {onBeforeUnmount, onMounted, ref} from "vue";
+import { getActiveShifts, getDashUsers } from "@/composables/fetchers.js";
+import { computed, onBeforeUnmount, onMounted, ref } from "vue";
 import UserCard from "@/components/pages/dash/UserCard.vue";
-import {useActiveShifts} from "@/stores/shiftStore.js";
+import { useActiveShifts } from "@/stores/shiftStore.js";
 import Alert from "@/components/Alert.vue";
 
-const activeShifts = useActiveShifts()
-
-const users = ref([])
+const activeShifts = useActiveShifts();
+const users = ref([]);
 const intervalId = ref(null);
 
-const checkActiveShift = async () => {
-  try{
-    const activeShiftsList = await getActiveShifts()
-    if (activeShiftsList) {
-      activeShifts.setActiveShifts(activeShiftsList)
-    }
+const activeUsers = computed(() => users.value.filter(user => !user.disabled));
 
+const checkActiveShift = async () => {
+  try {
+    const activeShiftsList = await getActiveShifts();
+    if (activeShiftsList) {
+      activeShifts.setActiveShifts(activeShiftsList);
+    }
   } catch (error) {
-    console.error("Error fetching getActiveSift:", error);
+    console.error("Error fetching getActiveShifts:", error);
   }
-}
+};
 
 onMounted(async () => {
   try {
     users.value = await getDashUsers();
-    await checkActiveShift()
+    await checkActiveShift();
+
     intervalId.value = setInterval(async () => {
-    await checkActiveShift()
-  }, 5000)
+      await checkActiveShift();
+    }, 5000);
   } catch (error) {
     console.error("Error fetching users and active shifts:", error);
   }
 });
 
 onBeforeUnmount(() => {
-  clearInterval(intervalId.value);
+  if (intervalId.value) {
+    clearInterval(intervalId.value);
+  }
 });
 </script>
 
@@ -45,7 +48,7 @@ onBeforeUnmount(() => {
   <div class="grid [grid-template-columns:repeat(auto-fit,minmax(120px,1fr))] gap-4">
 
     <UserCard
-        v-for="user in users"
+        v-for="user in activeUsers"
         :key="user.id"
         :user="user"
         :check-active-shift="checkActiveShift"
