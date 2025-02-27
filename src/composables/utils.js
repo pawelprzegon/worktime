4f24eaab9bdabc5cb150a881ccf735794055ec08
pawelprzegon <p.przegon@gmail.com>
2025-02-27 23:10:14 +0100

    fix: fixed VITE ENV name

--- src/composables/utils.js
+++ src/composables/utils.js
@@ -2,7 +2,7 @@ import {add, addDays, endOfMonth, getDay, startOfMonth, sub} from "date-fns";
 import {usedayStore} from "@/stores/calendarStore.js";
 
 export const url = import.meta.env.VITE_APP_API_URL
-export const webDev0 = import.meta.env.VITE_WEB_DEV_0
+export const webDev0 = import.meta.env.VITE_APP_WEB_DEV_0
 
 export const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
